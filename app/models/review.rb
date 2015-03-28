4fb6a2b91d596fd161175961ccd8f83ac34ec97e
Louis Pilfold <louis@lpil.uk>
2015-03-28 14:22:24 +0000

    Review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  validates :rating,
+            numericality: {
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to:    5
+            }
 end
