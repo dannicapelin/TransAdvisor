40ab40da5e4aa095a17c04c6f34668d21d382d73
Louis Pilfold <louis@lpil.uk>
2015-03-28 17:13:43 +0000

    Wire up the about and guide pageee

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,4 +2,12 @@ class PageController < ApplicationController
   def index
     render layout: false
   end
+
+  def about
+    render 'About.html'
+  end
+
+  def guides
+    render 'Guides.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'page#index'
+  get '/about', to: 'page#about'
+  get '/guides', to: 'page#guides'
 
   resources :institutions do
     resources :reviews, except: [:index]
