b4b9560e21f3a8c509ee072a00ad7c00d43e2d27
Louis Pilfold <louis@lpil.uk>
2015-03-28 14:03:18 +0000

    Institutions validations

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,14 @@
 class Institution < ActiveRecord::Base
+  validates :name,
+            presence: true
+
+  validates :address,
+            presence: true
+
+  validates :telephone,
+            length: { minimum: 8, maximum: 15 },
+            format: {
+              with: /\A[ \+\(\)\d]+\z/,
+              message: 'Must be a phone number'
+            }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 20150328133127) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
+
 end
